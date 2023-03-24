631f1d1e25d5185b9751798855e6d3e13fef619f
aarrunyal <aarrunyal@gmail.com>
2023-03-23 23:06:26 -0400

    mysql setup continue

--- bitcoin_tracker/settings.py
+++ bitcoin_tracker/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'bitcoin_tracker.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'mysql.connector.django',  
+        'NAME': 'django-demo',  
+        'USER': 'db',  
+        'PASSWORD': '',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306', 
     }
 }
 
